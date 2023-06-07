b4cba7b4da721bf48fab51cc15556c99e24c3d82
Younes Lahouiti <loller763@gmail.com>
2023-06-07 15:16:34 +0200

    Small change on hte test files

--- test/es6.js
+++ test/es6.js
@@ -7,3 +7,7 @@ const color = { hue: 200, saturation: 28, lightness: 35 };
 const converter = new ColorConverter("hsl", color);
 
 const hexColor = converter.convertTo("hex");
+console.log(hexColor);
+
+const allModels = converter.getAllColorModels();
+console.log(allModels);
