7a4a1a55aa8d581b0c1c911375b2cbeff86bd36e
Younes Lahouiti <loller763@gmail.com>
2023-06-07 11:10:27 +0200

    Addition of compiled js files for developers

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ColorConverter = exports.AbstractConversionMethods = void 0;
+var color_conversion_classes_1 = require("./classes/color-conversion.classes");
+Object.defineProperty(exports, "AbstractConversionMethods", { enumerable: true, get: function () { return color_conversion_classes_1.AbstractConversionMethods; } });
+Object.defineProperty(exports, "ColorConverter", { enumerable: true, get: function () { return color_conversion_classes_1.ColorConverter; } });
